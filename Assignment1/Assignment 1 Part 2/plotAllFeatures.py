0c9699ca8c31106962659afd2f5d04a1ad6229d9
Abilarsh Viji <AbilarshViji@users.noreply.github.com>
2020-10-06 22:36:50 -0400

    A1P2 Log Plots

--- Assignment1/Assignment 1 Part 2/plotAllFeatures.py
+++ Assignment1/Assignment 1 Part 2/plotAllFeatures.py
@@ -6,9 +6,9 @@ x, target = skds.load_boston(return_X_y=True)
 x = x.T
 for index, feature in enumerate(x):
     plt.figure(index)
-    plt.scatter(feature, target)
-    plt.title(skds.load_boston().feature_names[index] + " vs MEDV")
+    plt.scatter(np.log(feature), target)
+    plt.title("log(" + skds.load_boston().feature_names[index] + ") vs MEDV")
     plt.xlabel(skds.load_boston().feature_names[index])
     plt.ylabel("MEDV")
-    plt.savefig(skds.load_boston().feature_names[index]+".png")
+    plt.savefig(skds.load_boston().feature_names[index]+"log.png")
     
