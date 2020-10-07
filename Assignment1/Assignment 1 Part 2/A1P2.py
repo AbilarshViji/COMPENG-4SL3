b9bc0e695af99874eb8e670db484b12c34a3c989
Abilarsh Viji <AbilarshViji@users.noreply.github.com>
2020-10-06 21:23:48 -0400

    A1P2 cleaned up

--- Assignment1/Assignment 1 Part 2/A1P2.py
+++ Assignment1/Assignment 1 Part 2/A1P2.py
@@ -2,94 +2,98 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.datasets as skds
 import sklearn.model_selection as skms
-import sklearn.metrics as skmt
-import sklearn.linear_model as sklm
-import sklearn.metrics
-#https://cdn1.sph.harvard.edu/wp-content/uploads/sites/565/2018/08/572Spr07-BasisMethods-4up.pdf
-#https://chrispfchung.github.io/model%20data/boston-housing-data/#linear-relationships
 
 def computePrediction(x, w): #Computes prediction on a given set of x points
-    # pred = []
-    # # M = len(w) - 1
-    # pred = []
-    # for feature in x:
-    #     curr = 0
-    #     for m, value in enumerate(feature):
-    #         curr += w[m] * value
-    #     pred.append(curr)
-    # return pred
-    print(x, w)
     return np.dot(x, w)
-
 
 def computeWeights(trainingData, targetData):
     # print(trainingData)
-    A = np.dot(np.transpose(trainingData), trainingData)
-    c = np.dot(np.transpose(trainingData), targetData)
+    A = np.dot(trainingData.T, trainingData)
+    c = np.dot(trainingData.T, targetData)
     w = np.dot(np.linalg.inv(A), c)
-    # print('a', A)
-    # print('c', c)
-    # print(trainingData.shape, targetData.shape, A, c, w, sep="\n")
     return w
 
 def calcError(predictedList, actualList): #Computes least squares error
-    # error = 0
-    # for predict, actual in zip(predictedList, actualList): #Iterate through both predicted and actual list
-    #     error += (predict-actual)**2 #Sum difference squared
-    #     # print(predict-actual)
-    # return error/len(predictedList)
     diff = np.subtract(actualList, predictedList)
-    return np.dot(diff, np.transpose(diff))/len(diff)
+    return np.dot(diff, diff.T)/len(diff)
+
+def average(listToAverage):
+    return sum(listToAverage)/len(listToAverage)
 
 #import data set from scikit
 rand = 1727
 x, target = skds.load_boston(return_X_y=True)
-xIterate = x.transpose()
-errors = []
-# print(target)
-# # initialize error values
-# for featureIndex, feature in enumerate(xIterate):
-#     currentFoldError = []
-#     for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(feature):
-#         training = feature[train]
-#         targetTrain = target[train]
-#         w = computeWeights(training, targetTrain)
-        
-#         validation = feature[valid]
-#         targetValid = target[valid]
-#         validationOutput = computePrediction(validation, w)
-#         currentFoldError.append(skmt.r2_score(targetValid, validation))
-#         print(currentFoldError)
-#         # currentFoldError.append(calcError(validationOutput, targetValid))
-#     errors.append((featureIndex, np.average(currentFoldError)))
-# errors.sort(key=lambda x:x[1], reverse=True)
-# print('errors', errors)
+xIterate = x.T
 
 rankings = []
 for featureIndex, feature in enumerate(xIterate):
     rankings.append((featureIndex, np.corrcoef(target, feature)[0,1]))
 rankings.sort(key=lambda x:abs(x[1]), reverse=True)
 
-# print(rankings)
-# print(target)
-trainingSet = np.ones(506)
 KFoldError = []
-for index, error in rankings:
-    trainingSet = np.vstack((trainingSet, xIterate[index]))
-    w = computeWeights(np.transpose(trainingSet), target)
-    # print(w)
-    # w[0] = 0
-    # print(np.transpose(trainingSet)[0])
-    validationOutput = computePrediction(np.transpose(trainingSet), w) # TODO: VALIDATION SET
-    # print(validationOutput[:8])
-    KFoldError.append(np.sqrt(calcError(validationOutput, target)))
+KFoldBasisError = []
+testError = []
+testBasisError = []
+featureNamesOrder = ""
 
-    # trainingSet = np.vstack((trainingSet, xIterate[index]))
-    reg = sklm.LinearRegression().fit(np.transpose(trainingSet), target)
-    # print(reg.score(np.transpose(trainingSet), target))
-    # KFoldError.append((sklearn.metrics.mean_squared_error(target, reg.predict(np.transpose(trainingSet))))) #RMSE
-    # print(reg.coef_)
-    # x = input()
-print("hi",KFoldError)
-# print(target)
-# print(xIterate[3])
+xTrain, xTest, tTrain, tTest = skms.train_test_split(x, target, test_size = 1/4, random_state = rand)
+trainingSet = np.ones(np.size(xTrain,0))
+testingSet = np.ones(np.size(xTest,0))
+xTrain = xTrain.T
+xTest = xTest.T
+
+for index, error in rankings:
+
+    trainingSet = np.vstack((trainingSet, xTrain[index]))
+    testingSet = np.vstack((testingSet, xTest[index]))
+    currentCVError = []
+    currentCVBasisError = []
+    currentTestError = []
+    currentTestBasisError = []
+
+    featureNamesOrder += skds.load_boston().feature_names[index] + ","
+
+    for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(trainingSet.T):
+
+        # compute standard model
+        w = computeWeights(trainingSet.T[train], tTrain[train])
+        validationOutput = computePrediction(trainingSet.T[valid], w)
+        currentCVError.append(calcError(validationOutput, tTrain[valid]))
+
+        testOutput = computePrediction(testingSet.T, w)
+        currentTestError.append(calcError(testOutput, tTest))
+
+        #compute basis expansion model (ln)
+        logTrainingSet = np.copy(trainingSet)
+        for val in range(len(logTrainingSet[1])):
+            logTrainingSet[1][val] = np.log(logTrainingSet[1][val])            
+        w = computeWeights(logTrainingSet.T[train], tTrain[train])
+        validationBasisOutput = computePrediction(logTrainingSet.T[valid], w)
+        currentCVBasisError.append(calcError(validationBasisOutput, tTrain[valid]))
+
+        logTestingSet = np.copy(testingSet)
+        for val in range(len(logTestingSet[1])):
+            logTestingSet[1][val] = np.log(logTestingSet[1][val])
+        testBasisOutput = computePrediction(logTestingSet.T, w)
+        currentTestBasisError.append(calcError(testBasisOutput, tTest))
+        
+    KFoldError.append(average(currentCVError))
+    KFoldBasisError.append(average(currentCVBasisError))
+    testError.append(average(currentTestError))
+    testBasisError.append(average(currentTestBasisError))
+
+print(KFoldError)
+print(KFoldBasisError)
+print(testError)
+print(testBasisError)
+x = list(range(1, 14))
+plt.figure(0)
+plt.plot(x, KFoldError)
+plt.plot(x, KFoldBasisError)
+plt.plot(x, testError)
+plt.plot(x, testBasisError)
+plt.xlabel("k")
+plt.ylabel("Error")
+plt.title("Errors vs Size of Subset of Features (k)")
+plt.legend(["Cross-Validation Error", "Cross-Validation Basis Error", "Test Error", "Test Basis Error"])
+plt.show()
