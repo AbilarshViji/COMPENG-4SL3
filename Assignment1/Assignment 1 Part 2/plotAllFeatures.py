0a9b1b61d206a01192f12da0414099abc51bfcc0
Abilarsh Viji <AbilarshViji@users.noreply.github.com>
2020-10-06 21:24:08 -0400

    Plotting code and plots for each variable

---
+++ Assignment1/Assignment 1 Part 2/plotAllFeatures.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sklearn.datasets as skds
+import matplotlib.pyplot as plt
+
+x, target = skds.load_boston(return_X_y=True)
+x = x.T
+for index, feature in enumerate(x):
+    plt.figure(index)
+    plt.scatter(feature, target)
+    plt.title(skds.load_boston().feature_names[index] + " vs MEDV")
+    plt.xlabel(skds.load_boston().feature_names[index])
+    plt.ylabel("MEDV")
+    plt.savefig(skds.load_boston().feature_names[index]+".png")
+    
