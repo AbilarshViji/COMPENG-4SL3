c526a2185bede899b6f1f35629dd028cadf1aa67
Abilarsh Viji <AbilarshViji@users.noreply.github.com>
2020-10-10 14:51:34 -0400

    Comment A1P2

--- Assignment1/Assignment 1 Part 2/A1P2.py
+++ Assignment1/Assignment 1 Part 2/A1P2.py
@@ -6,8 +6,7 @@ import sklearn.model_selection as skms
 def computePrediction(x, w): #Computes prediction on a given set of x points
     return np.dot(x, w)
 
-def computeWeights(trainingData, targetData):
-    # print(trainingData)
+def computeWeights(trainingData, targetData): #Computes weights
     A = np.dot(trainingData.T, trainingData)
     c = np.dot(trainingData.T, targetData)
     w = np.dot(np.linalg.inv(A), c)
@@ -17,10 +16,10 @@ def calcError(predictedList, actualList): #Computes least squares error
     diff = np.subtract(actualList, predictedList)
     return np.dot(diff, diff.T)/len(diff)
 
-def average(listToAverage):
+def average(listToAverage): #Computes average
     return sum(listToAverage)/len(listToAverage)
 
-def trainAndGetError(training, valid, target, validTarget):
+def trainAndGetError(training, valid, target, validTarget): #Computes weight, prediction, and error
     w = computeWeights(training, target)
     validationOutput = computePrediction(valid, w)
     return calcError(validationOutput, validTarget)
@@ -28,116 +27,66 @@ def trainAndGetError(training, valid, target, validTarget):
 #import data set from scikit
 rand = 1727
 x, target = skds.load_boston(return_X_y=True)
-xIterate = x.T
 
-# KFoldError = []
-KFoldBasisError = []
-testError = []
-testBasisError = []
-featureNamesOrder = ""
-
+#Splits training and testing set
 xTrain, xTest, tTrain, tTest = skms.train_test_split(x, target, test_size = 1/4, random_state = rand)
-trainingSet = np.ones(np.size(xTrain,0))
-testingSet = np.ones(np.size(xTest,0))
+trainingSet = np.ones(np.size(xTrain,0)) #Initalize training dummy
+testingSet = np.ones(np.size(xTest,0)) #Initialize testing dummy
 xTrain = xTrain.T
 xTest = xTest.T
 
-rankings = []
+rankings = [] 
 featuresRemaining = list(range(0, 13))
-# for k in range(13, 0, -1):
 crossValidationError = []
 crossValidationBasisError = []
 testingError = []
 testingBasisError = []
   
-#Crossfold non basis
-while featuresRemaining:
+# Determine best feature, calculate KFold and testing errors
+while featuresRemaining: #Runs while features are remaining
     foldError = []
-    for feature in featuresRemaining:
+    for feature in featuresRemaining: #Iterate over each remaining feature
         currentError = []
-        for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(trainingSet.T):
+        for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(trainingSet.T): #Iterate over 5 KFoldes
             current = np.vstack((trainingSet, xTrain[feature]))
             currentError.append(trainAndGetError(current.T[train], current.T[valid], tTrain[train], tTrain[valid]))
-        foldError.append((feature, average(currentError)))
+        foldError.append((feature, average(currentError))) #Append error for current feature
     
-    foldError.sort(key=lambda x:x[1])
-    bestFeature = foldError[0][0]
-    crossValidationError.append(foldError[0][1])
-    trainingSet = np.vstack((trainingSet, xTrain[bestFeature]))
+    foldError.sort(key=lambda x:x[1]) #Sort list by error
+    bestFeature = foldError[0][0] #Save best feature
+    crossValidationError.append(foldError[0][1]) #Save best error
+    trainingSet = np.vstack((trainingSet, xTrain[bestFeature])) #Add best feature to training test
     
-    testingSet = np.vstack((testingSet, xTest[bestFeature]))
-    testingError.append(trainAndGetError(trainingSet.T, testingSet.T, tTrain, tTest))
+    testingSet = np.vstack((testingSet, xTest[bestFeature])) #Add best feature to testing set
+    testingError.append(trainAndGetError(trainingSet.T, testingSet.T, tTrain, tTest)) #Compute testing error
     
-    rankings.append(bestFeature)
-    featuresRemaining.remove(bestFeature)
+    rankings.append(bestFeature) #Save best feature
+    featuresRemaining.remove(bestFeature) #Remove best feature from features remaining
 
-print("RANMFLKNALNLFNS", rankings, crossValidationError, testingError)
-# trainingSet = np.ones(np.size(xTrain.T,0))
-# testingSet = np.ones(np.size(xTest.T,0))
+# Compute basis KFold and testing error
+logTrain = trainingSet[0]
+logTest = testingSet[0]
 
-for k in range(2,len(trainingSet)):
-    logTrain = np.vstack((trainingSet[0], (np.sqrt(trainingSet[1:k]))))
-    logTest = np.vstack((testingSet[0], (np.sqrt(testingSet[1:k]))))
-    # print(k, logTrain)
-    for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(logTrain.T):
-        currentError.append(trainAndGetError(logTrain.T[train], logTrain.T[valid], tTrain[train], tTrain[valid]))
-    crossValidationBasisError.append(average(currentError))
-    testingBasisError.append(trainAndGetError(logTrain.T, logTest.T, tTrain, tTest))
+for k in range(1,len(trainingSet)):
+    if 0 in trainingSet[k] or 0 in testingSet[k]: #Log(0) = inf, so if 0 is in feature, do not apply log
+        logTrain = np.vstack((logTrain, trainingSet[k]))
+        logTest = np.vstack((logTest, testingSet[k]))
+    else: #else apply log function to feature
+        logTrain = np.vstack((logTrain, np.log(trainingSet[k])))
+        logTest = np.vstack((logTest, np.log(testingSet[k])))
+    for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(logTrain.T): #Iterate over 5 KFolds
+        currentError.append(trainAndGetError(logTrain.T[train], logTrain.T[valid], tTrain[train], tTrain[valid])) #Get errors for current feature
+    crossValidationBasisError.append(average(currentError)) #Append average error (cross validation error for basis function)
+    testingBasisError.append(trainAndGetError(logTrain.T, logTest.T, tTrain, tTest)) #Compute and save testing error
 
-print(crossValidationBasisError)
-print(testingBasisError)
-# for index, error in enumerate(rankings):
-
-#     trainingSet = np.vstack((trainingSet, xTrain[index]))
-#     testingSet = np.vstack((testingSet, xTest[index]))
-#     currentCVError = []
-#     currentCVBasisError = []
-#     currentTestError = []
-#     currentTestBasisError = []
-
-#     featureNamesOrder += skds.load_boston().feature_names[index] + ","
-
-#     for train, valid in skms.KFold(5, shuffle=True, random_state=rand).split(trainingSet.T):
-
-#         # compute standard model
-#         w = computeWeights(trainingSet.T[train], tTrain[train])
-#         validationOutput = computePrediction(trainingSet.T[valid], w)
-#         currentCVError.append(calcError(validationOutput, tTrain[valid]))
-
-#         testOutput = computePrediction(testingSet.T, w)
-#         currentTestError.append(calcError(testOutput, tTest))
-
-#         #compute basis expansion model (ln)
-#         logTrainingSet = np.copy(trainingSet)
-#         for val in range(len(logTrainingSet[1])):
-#             logTrainingSet[1][val] = np.log(logTrainingSet[1][val])            
-#         w = computeWeights(logTrainingSet.T[train], tTrain[train])
-#         validationBasisOutput = computePrediction(logTrainingSet.T[valid], w)
-#         currentCVBasisError.append(calcError(validationBasisOutput, tTrain[valid]))
-
-#         logTestingSet = np.copy(testingSet)
-#         for val in range(len(logTestingSet[1])):
-#             logTestingSet[1][val] = np.log(logTestingSet[1][val])
-#         testBasisOutput = computePrediction(logTestingSet.T, w)
-#         currentTestBasisError.append(calcError(testBasisOutput, tTest))
-        
-#     KFoldError.append(average(currentCVError))
-#     KFoldBasisError.append(average(currentCVBasisError))
-#     testError.append(average(currentTestError))
-#     testBasisError.append(average(currentTestBasisError))
-
-# print(KFoldError)
-# print(KFoldBasisError)
-# print(testError)
-# print(testBasisError)
-# x = list(range(1, 1))
-# plt.figure(0)
-# plt.plot(x, KFoldError)
-# plt.plot(x, KFoldBasisError)
-# plt.plot(x, testError)
-# plt.plot(x, testBasisError)
-# plt.xlabel("k")
-# plt.ylabel("Error")
-# plt.title("Errors vs Size of Subset of Features (k)")
-# plt.legend(["Cross-Validation Error", "Cross-Validation Basis Error", "Test Error", "Test Basis Error"])
-# plt.show()
+k = list(range(1, 14)) #Initialize k for graphing
+plt.figure(0)
+plt.plot(k, crossValidationError)
+plt.plot(k, crossValidationBasisError)
+plt.plot(k, testingError)
+plt.plot(k, testingBasisError)
+plt.xlabel("k")
+plt.ylabel("Error")
+plt.title("Errors vs Size of Subset of Features (k)")
+plt.legend(["Cross-Validation Error", "Cross-Validation Basis Error", "Test Error", "Test Basis Error"])
+plt.show()
